f1d6b49ba9e00cf66fd6c86f321b14a3744b3492
Manu T S <manu.ts@outlook.com>
2015-05-13 17:54:57 +0200

    adding ofdm modem 	modified:   examples/ofdmtxrx.cc 	new file:   include/ofdm_modem.h 	new file:   lib/ofdm_modem.cc

---
+++ include/ofdm_modem.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2014, 2015 Manu T S
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OFDM_MODEM_H
+#define OFDM_MODEM_H
+
+// Read data to be transmitted. Modulate it and write to the 
+// tx buffer
+class ofdm_mod();
+
+// Read data from rx buffer. Demodulate it and store data.
+class ofdm_dem();
+
+#endif
